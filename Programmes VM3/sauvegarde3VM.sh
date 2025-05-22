bfe30ad729d8ad36918f80ac84c2673662260146
xXAgentMUXx <mathys.urban@ynov.com>
2025-05-22 09:34:42 +0000

    Update sauvegarde3VM.sh

--- Programmes VM3/sauvegarde3VM.sh
+++ Programmes VM3/sauvegarde3VM.sh
@@ -4,15 +4,16 @@ SRC="/home/vboxuser/Documents/"
 TMP_DIR="/tmp/backup_split"
 PART1="$TMP_DIR/part1"
 PART2="$TMP_DIR/part2"
-DEST1="vboxuser@192.168.56.101:/home/vboxuser/sauvegardes2/"
-DEST2="vboxuser@192.168.56.102:/home/vboxuser/sauvegardes2/"
+DEST1="vboxuser@192.168.56.101:/home/vboxuser/sauvegardes3/"  # VM1
+DEST2="vboxuser@192.168.56.102:/home/vboxuser/sauvegardes3/"  # VM2
+DEST3="vboxuser@192.168.56.103:/home/vboxuser/sauvegarde_complet3/"  # VM3 (sauvegarde complète)
 
-# Nettoyage et préparation
+# Nettoyage
 echo "[INFO] Préparation du dossier temporaire..."
 rm -rf "$TMP_DIR"
 mkdir -p "$PART1" "$PART2"
 
-# Répartition des fichiers
+# Division des fichiers
 i=0
 for file in "$SRC"*; do
     if [ $((i % 2)) -eq 0 ]; then
@@ -23,171 +24,13 @@ for file in "$SRC"*; do
     i=$((i + 1))
 done
 
-# Envoi direct des fichiers vers les VMs
+# Sauvegardes réparties
 rsync -avz "$PART1/" "$DEST1"
 rsync -avz "$PART2/" "$DEST2"
 
-# Nettoyage final
+# Sauvegarde complète sur VM3
+rsync -avz "$SRC" "$DEST3"
+
+# Nettoyage
 rm -rf "$TMP_DIR"
-echo "✅ Sauvegarde divisée envoyée sur VM1 et VM2."
-#!/bin/bash
-
-SRC="/home/vboxuser/Documents/"
-TMP_DIR="/tmp/backup_split"
-PART1="$TMP_DIR/part1"
-PART2="$TMP_DIR/part2"
-DEST1="vboxuser@192.168.56.102:/home/vboxuser/sauvegardes/"
-DEST2="vboxuser@192.168.56.103:/home/vboxuser/sauvegardes/"
-
-# Nettoyage et préparation
-echo "[INFO] Préparation du dossier temporaire..."
-rm -rf "$TMP_DIR"
-mkdir -p "$PART1" "$PART2"
-
-# Répartition des fichiers
-i=0
-for file in "$SRC"*; do
-    if [ $((i % 2)) -eq 0 ]; then
-        cp -r "$file" "$PART1/"
-    else
-        cp -r "$file" "$PART2/"
-    fi
-    i=$((i + 1))
-done
-
-# Envoi direct des fichiers vers les VMs
-rsync -avz "$PART1/" "$DEST1"
-rsync -avz "$PART2/" "$DEST2"
-
-# Nettoyage final
-rm -rf "$TMP_DIR"
-echo "✅ Sauvegarde divisée envoyée sur VM2 et VM3."
-#!/bin/bash
-
-SRC="/home/vboxuser/Documents/"
-TMP_DIR="/tmp/backup_split"
-PART1="$TMP_DIR/part1"
-PART2="$TMP_DIR/part2"
-DEST1="vboxuser@192.168.56.102:/home/vboxuser/sauvegardes/"
-DEST2="vboxuser@192.168.56.103:/home/vboxuser/sauvegardes/"
-
-# Nettoyage et préparation
-echo "[INFO] Préparation du dossier temporaire..."
-rm -rf "$TMP_DIR"
-mkdir -p "$PART1" "$PART2"
-
-# Répartition des fichiers
-i=0
-for file in "$SRC"*; do
-    if [ $((i % 2)) -eq 0 ]; then
-        cp -r "$file" "$PART1/"
-    else
-        cp -r "$file" "$PART2/"
-    fi
-    i=$((i + 1))
-done
-
-# Envoi direct des fichiers vers les VMs
-rsync -avz "$PART1/" "$DEST1"
-rsync -avz "$PART2/" "$DEST2"
-
-# Nettoyage final
-rm -rf "$TMP_DIR"
-echo "✅ Sauvegarde divisée envoyée sur VM2 et VM3."
-#!/bin/bash
-
-SRC="/home/vboxuser/Documents/"
-TMP_DIR="/tmp/backup_split"
-PART1="$TMP_DIR/part1"
-PART2="$TMP_DIR/part2"
-DEST1="vboxuser@192.168.56.102:/home/vboxuser/sauvegardes/"
-DEST2="vboxuser@192.168.56.103:/home/vboxuser/sauvegardes/"
-
-# Nettoyage et préparation
-echo "[INFO] Préparation du dossier temporaire..."
-rm -rf "$TMP_DIR"
-mkdir -p "$PART1" "$PART2"
-
-# Répartition des fichiers
-i=0
-for file in "$SRC"*; do
-    if [ $((i % 2)) -eq 0 ]; then
-        cp -r "$file" "$PART1/"
-    else
-        cp -r "$file" "$PART2/"
-    fi
-    i=$((i + 1))
-done
-
-# Envoi direct des fichiers vers les VMs
-rsync -avz "$PART1/" "$DEST1"
-rsync -avz "$PART2/" "$DEST2"
-
-# Nettoyage final
-rm -rf "$TMP_DIR"
-echo "✅ Sauvegarde divisée envoyée sur VM2 et VM3."
-#!/bin/bash
-
-SRC="/home/vboxuser/Documents/"
-TMP_DIR="/tmp/backup_split"
-PART1="$TMP_DIR/part1"
-PART2="$TMP_DIR/part2"
-DEST1="vboxuser@192.168.56.102:/home/vboxuser/sauvegardes/"
-DEST2="vboxuser@192.168.56.103:/home/vboxuser/sauvegardes/"
-
-# Nettoyage et préparation
-echo "[INFO] Préparation du dossier temporaire..."
-rm -rf "$TMP_DIR"
-mkdir -p "$PART1" "$PART2"
-
-# Répartition des fichiers
-i=0
-for file in "$SRC"*; do
-    if [ $((i % 2)) -eq 0 ]; then
-        cp -r "$file" "$PART1/"
-    else
-        cp -r "$file" "$PART2/"
-    fi
-    i=$((i + 1))
-done
-
-# Envoi direct des fichiers vers les VMs
-rsync -avz "$PART1/" "$DEST1"
-rsync -avz "$PART2/" "$DEST2"
-
-# Nettoyage final
-rm -rf "$TMP_DIR"
-echo "✅ Sauvegarde divisée envoyée sur VM2 et VM3."
-#!/bin/bash
-
-SRC="/home/vboxuser/Documents/"
-TMP_DIR="/tmp/backup_split"
-PART1="$TMP_DIR/part1"
-PART2="$TMP_DIR/part2"
-DEST1="vboxuser@192.168.56.102:/home/vboxuser/sauvegardes/"
-DEST2="vboxuser@192.168.56.103:/home/vboxuser/sauvegardes/"
-
-# Nettoyage et préparation
-echo "[INFO] Préparation du dossier temporaire..."
-rm -rf "$TMP_DIR"
-mkdir -p "$PART1" "$PART2"
-
-# Répartition des fichiers
-i=0
-for file in "$SRC"*; do
-    if [ $((i % 2)) -eq 0 ]; then
-        cp -r "$file" "$PART1/"
-    else
-        cp -r "$file" "$PART2/"
-    fi
-    i=$((i + 1))
-done
-
-# Envoi direct des fichiers vers les VMs
-rsync -avz "$PART1/" "$DEST1"
-rsync -avz "$PART2/" "$DEST2"
-
-# Nettoyage final
-rm -rf "$TMP_DIR"
-echo "✅ Sauvegarde divisée envoyée sur VM2 et VM3."
-
+echo "✅ Sauvegarde divisée sur VM1/VM2 et complète sur VM3."
